b1991f44a21633241d1de7a88f110e86ef382955
Jason Kincl <jkincl@gmail.com>
2022-10-04 22:10:20 -0400

    Initial cgo implementation working with hdhr discovery

--- main.go
+++ main.go
@@ -7,15 +7,48 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"net/netip"
+	"os"
 	"os/exec"
+	"strconv"
+	"unsafe"
 )
 
+// #cgo CFLAGS: -I/Users/jasonkincl/Workspace/libhdhomerun
+// #cgo LDFLAGS: -lpthread -L/Users/jasonkincl/Workspace/libhdhomerun -lhdhomerun
+// #include <stdio.h>
+// #include <stdbool.h>
+// #include <stdint.h>
+// #include "hdhomerun.h"
+import "C"
+
+// inet_ntoa came from https://go.dev/play/p/JlYJXZnUxl
+func inet_ntoa(ipInt64 uint32) (ip netip.Addr) {
+	ipArray := [4]byte{byte(ipInt64 >> 24), byte(ipInt64 >> 16), byte(ipInt64 >> 8), byte(ipInt64)}
+	ip = netip.AddrFrom4(ipArray)
+	return
+}
+
 func main() {
+	wildcard, _ := strconv.ParseInt("0xFFFFFFFF", 0, 64)
+	ptr := C.malloc(C.sizeof_struct_hdhomerun_discover_device_t)
+	defer C.free(unsafe.Pointer(ptr))
+
+	numFound := C.hdhomerun_discover_find_devices_custom_v2(
+		C.uint(0),
+		C.uint(wildcard),
+		C.uint(wildcard),
+		(*C.struct_hdhomerun_discover_device_t)(ptr),
+		1)
+
+	if numFound == 0 {
+		fmt.Println("Did not find any devices! Exiting")
+		os.Exit(1)
+	}
+	device := (*C.struct_hdhomerun_discover_device_t)(ptr)
+	fmt.Printf("Found %d HDHR device: %X %s\n", numFound, device.device_id, inet_ntoa((uint32)(device.ip_addr)))
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// go func() {
-		// 	<-r.Context().Done()
-		// 	fmt.Println("Done")
-		// }()
 		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
 
 		addr := net.UDPAddr{
@@ -29,8 +62,6 @@ func main() {
 			return
 		}
 		defer conn.Close()
-
-		// var buf [1358]byte
 		rconn := bufio.NewReader(conn)
 
 		flusher, ok := w.(http.Flusher)
@@ -48,28 +79,20 @@ func main() {
 		}
 
 		for {
-
 			select {
 			case <-r.Context().Done():
 				fmt.Println("Connection closed, releasing UDP :6000")
 				return
 			default:
-				// fmt.Fprintf(w, "Chunk #%d\n", i)
-				// _, _, err := conn.ReadFromUDP(buf[:])
 				if err != nil {
 					fmt.Println("error reading from UDP:", err.Error())
 				}
-				// fmt.Println("read bytes:", num)
-				// w.Write(buf[:])
 				io.CopyN(w, rconn, 1500)
-
-				flusher.Flush() // Trigger "chunked" encoding and send a chunk...
-				// time.Sleep(500 * time.Millisecond)
+				flusher.Flush() // Trigger "chunked" encoding and send a chunk
 			}
 		}
-
 	})
 
-	log.Print("Listening on localhost:8080")
+	log.Print("Listening on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
