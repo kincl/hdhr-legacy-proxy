023151c82e73e1a5bdbd31da3a27f4d9e35fca2a
Jason Kincl <jkincl@gmail.com>
2023-09-04 20:18:11 -0400

    bugfix: if we have a stream go for it

--- internal/proxy/http.go
+++ internal/proxy/http.go
@@ -172,6 +172,9 @@ func (proxy *Proxy) stream(w http.ResponseWriter, r *http.Request, ps httprouter
 	for i := 0; i < len(proxy.devices); i++ {
 		log.Printf("trying to stream from tuner %d\n", i)
 		stream, err = proxy.devices[i].GetStream(ps.ByName("channel"), ps.ByName("program"), fmt.Sprintf("%s:%s", proxy.Hostname, proxy.TunerPort))
+		if err == nil {
+			break
+		}
 		if err != nil && !errors.Is(err, errors.New("device in use")) {
 			log.Printf("stream error: %v", err)
 		}
